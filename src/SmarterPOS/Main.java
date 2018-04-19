3a53d678a0374d49db0200761aeef007e8253421
Feras Alawadi <ferasawady@gmail.com>
2018-04-19 21:25:29 +0300

    3 Commet

--- src/SmarterPOS/Main.java
+++ src/SmarterPOS/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Layout/Login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
     }
 
